e5f10f149139e340ab3991a2807523bf8be7d3a9
marouen maatoug <marouen.gimii@gmail.com>
2023-02-07 17:31:04 +0100

    Hero & Programs components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import './App.css';
+import "./App.css";
+import Hero from "./components/Hero/Hero";
+import Programs from "./components/Programs/Programs";
 function App() {
-  return (
-    <div className="App">
-          <span style={{color: 'white'}}>Subscribe Zainkeepscode</span>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Hero />
+            <Programs />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import Header from "../Header/Header";
+import "./Hero.css";
+import hero_image from "../../assets/hero_image.png";
+import hero_image_back from "../../assets/hero_image_back.png";
+import heart from "../../assets/heart.png";
+import calories from "../../assets/calories.png";
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            <div className="left-h">
+                <Header />
+                {/* The best ad */}
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>the best fitness club in the town</span>
+                </div>
+                {/* Hero heading */}
+                <div className="hero-text">
+                    <div>
+                        <span className="stroke-text">shape </span>
+                        <span>your</span>
+                    </div>
+                    <div>
+                        <span>ideal body</span>
+                    </div>
+                    <div>
+                        <span>
+                            in here we will help you to shape and build your ideal body
+                            and live up your life to fullest
+                        </span>
+                    </div>
+                </div>
+                {/* Figures */}
+                <div className="figures">
+                    <div>
+                        <span>+140</span>
+                        <span>expert coachs</span>
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>members joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
+                </div>
+                {/* hero buttons */}
+                <div className="hero-buttons">
+                    <button className="btn">Get started</button>
+                    <button className="btn">Learn more</button>
+                </div>
+            </div>
+            <div className="right-h">
+                <button className="btn">Join Now</button>
+                <div className="heart-rate">
+                    <img src={heart} alt="" />
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </div>
+                {/* hero images */}
+                <img src={hero_image} alt="" className="hero-image" />
+                <img src={hero_image_back} alt="" className="hero-image-back" />
+                {/* clories */}
+                <div className="calories">
+                    <img src={calories} alt="" />
+                    <div>
+                        <span>calories Burned</span>
+                        <span>220 kcal</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Hero;

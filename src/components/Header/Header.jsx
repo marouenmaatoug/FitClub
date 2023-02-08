e3c55eed8019541a1571556a1957e26c780c317b
marouen maatoug <marouen.gimii@gmail.com>
2023-02-08 18:42:50 +0100

    Responive - Mobile view

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
+import Bars from "../../assets/bars.png";
 
 const Header = () => {
+    const mobile = window.innerWidth <= 768 ? true : false;
+    const [menuOpened, setMenuOpened] = useState(false);
     return (
         <div className="header">
             <img src={Logo} alt="" className="logo" />
-            <ul className="header-menu">
-                <li>Home</li>
-                <li>Programs</li>
-                <li>Why us</li>
-                <li>Plans</li>
-                <li>Testimonials</li>
-            </ul>
+            {menuOpened === false && mobile === true ? (
+                <div onClick={() => setMenuOpened(true)}>
+                    <img className="bars" src={Bars} alt="" />
+                </div>
+            ) : (
+                <ul className="header-menu">
+                    <li onClick={() => setMenuOpened(false)}>Programs</li>
+                    <li onClick={() => setMenuOpened(false)}>Why us</li>
+                    <li onClick={() => setMenuOpened(false)}>Home</li>
+                    <li onClick={() => setMenuOpened(false)}>Plans</li>
+                    <li onClick={() => setMenuOpened(false)}>Testimonials</li>
+                </ul>
+            )}
         </div>
     );
 };

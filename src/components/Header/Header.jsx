ed4a7b7c1cb7d6d3abf3f8ca00398cb095dacc30
marouen maatoug <marouen.gimii@gmail.com>
2023-02-08 19:49:53 +0100

    Number Counter

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Header.css";
 import Logo from "../../assets/logo.png";
 import Bars from "../../assets/bars.png";
+import { Link } from "react-scroll";
 
 const Header = () => {
     const mobile = window.innerWidth <= 768 ? true : false;
@@ -10,16 +11,70 @@ const Header = () => {
         <div className="header">
             <img src={Logo} alt="" className="logo" />
             {menuOpened === false && mobile === true ? (
-                <div onClick={() => setMenuOpened(true)}>
-                    <img className="bars" src={Bars} alt="" />
+                <div>
+                    <img
+                        className="bars"
+                        src={Bars}
+                        alt=""
+                        onClick={() => setMenuOpened(true)}
+                    />
                 </div>
             ) : (
                 <ul className="header-menu">
-                    <li onClick={() => setMenuOpened(false)}>Programs</li>
-                    <li onClick={() => setMenuOpened(false)}>Why us</li>
-                    <li onClick={() => setMenuOpened(false)}>Home</li>
-                    <li onClick={() => setMenuOpened(false)}>Plans</li>
-                    <li onClick={() => setMenuOpened(false)}>Testimonials</li>
+                    <li>
+                        {" "}
+                        <Link
+                            onClick={() => setMenuOpened(false)}
+                            to="home"
+                            smooth={true}
+                            span={true}
+                            activeClass="active"
+                        >
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        {" "}
+                        <Link
+                            onClick={() => setMenuOpened(false)}
+                            to="programs"
+                            smooth={true}
+                            span={true}
+                        >
+                            Programs
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={() => setMenuOpened(false)}
+                            to="reasons"
+                            smooth={true}
+                            span={true}
+                        >
+                            Why us
+                        </Link>
+                    </li>
+                    <li>
+                        {" "}
+                        <Link
+                            onClick={() => setMenuOpened(false)}
+                            to="plans"
+                            smooth={true}
+                            span={true}
+                        >
+                            Plans
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={() => setMenuOpened(false)}
+                            to="testimonials"
+                            smooth={true}
+                            span={true}
+                        >
+                            Testimonials
+                        </Link>
+                    </li>
                 </ul>
             )}
         </div>
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,12 +6,13 @@ import hero_image_back from "../../assets/hero_image_back.png";
 import heart from "../../assets/heart.png";
 import calories from "../../assets/calories.png";
 import { motion } from "framer-motion";
+import NumberCounter from "number-counter";
 
 const Hero = () => {
     const transition = { type: "spring", duration: 3 };
     const mobile = window.innerWidth <= 768 ? true : false;
     return (
-        <div className="hero">
+        <div className="hero" id="home">
             <div className="blur hero-blur"></div>
             <div className="left-h">
                 <Header />
@@ -43,15 +44,21 @@ const Hero = () => {
                 {/* Figures */}
                 <div className="figures">
                     <div>
-                        <span>+140</span>
+                        <span>
+                            <NumberCounter end={140} start={100} delay="4" preFix="+" />
+                        </span>
                         <span>expert coachs</span>
                     </div>
                     <div>
-                        <span>+978</span>
+                        <span>
+                            <NumberCounter end={978} start={800} delay="4" preFix="+" />
+                        </span>
                         <span>members joined</span>
                     </div>
                     <div>
-                        <span>+50</span>
+                        <span>
+                            <NumberCounter end={50} start={0} delay="4" preFix="+" />
+                        </span>
                         <span>fitness programs</span>
                     </div>
                 </div>
